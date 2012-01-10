09182374b08f135ed2db9321e22c35d804bcd11d
vvakame <vvakame+dev@gmail.com>
2012-01-11 00:56:21 +0900

    categories.js に少しコメントを追加.

--- war/js/categories.js
+++ war/js/categories.js
@@ -10,13 +10,15 @@ $(document).ready(function(){
     getCategories(function(categoryList){
         for (var i=0; i<categoryList.length; i++){
             var category = categoryList[i];
-            var categoryName = category["name"];
+            var categoryName = category.name;
             console.log("categoryName:" + categoryName );
             addCategoryToDom(category);
         }
         tmpAddButtonAdd();
     });
 });
+
+// TODO JsDoc でぐぐれ！ Javadocっぽい書き方よりJsDocのほうがいいよ！
 
 /**
  * カテゴリー一覧を取得しにいく
@@ -27,7 +29,6 @@ var getCategories = function(callback) {
         "/category",
         {
             type:"GET",
-//            dataType:'jsonp',
             success:callback,
             error:function(){
                 console.log(aguments);
@@ -46,6 +47,7 @@ var addCategoryToDom = function(category) {
 
     // TODO main.cssの"#category_list li a{"の箇所、別のclass名振ってください...
     // リンク貼りたくないので...
+    // ↑自分で勝手に変えなさい！( ･`ω･´)
     var categoryNameEl = $("<li id=\"" + category.name + "\"/>");
     categoryNameEl.click(function(){
         onClickCategory(category.name);
@@ -58,6 +60,7 @@ var addCategoryToDom = function(category) {
 
 /**
  * ただのスタブ... あとで消すよこれ
+ * ↑はよ消せｗｗｗ
  */
 var tmpAddButtonAdd = function() {
     var parent = $("#category_list");
@@ -70,4 +73,23 @@ var tmpAddButtonAdd = function() {
 
     parent.append(categoryNameEl);
 
-}
+};
+
+// わかめならこう書く
+// JavaScriptの場合、かっしーが書いたみたいに引数に関数を渡す場合冗長になりがち。
+// そこで、一回名前を与えてからそっちを引数として渡したほうが分かりやすい場合のほうが多い気がするかなー。
+/*
+$(document).ready(function(){
+    var processCategories = function(categoryList){
+        for (var i=0; i<categoryList.length; i++){
+            var category = categoryList[i];
+            var categoryName = category.name;
+            console.log("categoryName:" + categoryName );
+            addCategoryToDom(category);
+        }
+    };
+
+    getCategories(processCategories);
+    tmpAddButtonAdd();
+});
+*/
